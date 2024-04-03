b63961147546490325c352c9a9cefa6482e1abfd
Carlos <carsandeu@gmail.com>
2024-04-03 13:50:15 +0200

    resizing R and changin a bit the README

--- resize/resize-svg.py
+++ resize/resize-svg.py
@@ -6,6 +6,7 @@ print("******************************")
 # if __name__ == "__main__":
 logos = os.listdir("./logos")
 for file in logos:
+    print("Resizing ", file)
     path = os.path.join("./logos", file)
     fig = sg.fromfile(path)
     fig.set_size(("100", "100"))
